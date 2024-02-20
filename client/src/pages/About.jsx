b2d829ac7ad0a7d1ee4ecfdae1619086d168fb4a
Palinlin <ibson.palinlin@accenture.com>
2024-02-20 11:33:25 +0800

    Complete sign in page UI

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,27 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold  mb-4 text-slate-800">
+        Nuriko Inventory App
+      </h1>
+      <p className="mb-4 text-slate-700">
+        The inventory app is a web application that allows users to manage and
+        track their inventory. It provides features such as adding new items,
+        updating existing items, and viewing the current inventory.
+      </p>
+      <p className="mb-4 text-slate-700">
+        The app provides a user-friendly interface with intuitive controls for
+        easy navigation and management of inventory items. Users can easily
+        search for specific items, sort them by various criteria, and perform
+        bulk actions such as deleting or exporting items.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Additionally, the app includes advanced features like generating
+        reports, setting up notifications for low stock items, and integrating
+        with external systems for seamless inventory management.
+      </p>
+    </div>
+  );
 }
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
-export default function SignOut() {
-  return <div>SignOut</div>;
+export default function SignIn() {
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
